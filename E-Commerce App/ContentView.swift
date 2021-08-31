28dbd1196dff6a07a1ecdb259ec700404a92c9ea
Justin747 <justin747@gmail.com>
2021-08-30 17:43:31 -0700

    Initial Commit

---
+++ E-Commerce App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  E-Commerce App
+//
+//  Created by Justin747 on 8/30/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ E-Commerce App/E_Commerce_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_Commerce_AppApp.swift
+//  E-Commerce App
+//
+//  Created by Justin747 on 8/30/21.
+//
+
+import SwiftUI
+
+@main
+struct E_Commerce_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
