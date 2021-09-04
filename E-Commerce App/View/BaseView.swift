15d1fcf69ec4fefd37641e3c56a66a45e4cdf2d4
Justin747 <justin747@gmail.com>
2021-09-03 23:50:37 -0700

    Added BaseViewModel
    Added BaseViewModel for Tab Bar items

---
+++ E-Commerce App/View/BaseView.swift
@@ -0,0 +1,20 @@
+//
+//  BaseView.swift
+//  BaseView
+//
+//  Created by Justin747 on 9/3/21.
+//
+
+import SwiftUI
+
+struct BaseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BaseView_Previews: PreviewProvider {
+    static var previews: some View {
+        BaseView()
+    }
+}
---
+++ E-Commerce App/ViewModel/BaseViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  BaseViewModel.swift
+//  BaseViewModel
+//
+//  Created by Justin747 on 9/3/21.
+//
+
+import SwiftUI
+
+class BaseViewModel: ObservableObject {
+    
+    //MARK: - Tab Bar
+    
+    @Published var currentTab: Tab = .Home
+    
+}
+
+//MARK: - Enum Case for Tab
+
+enum Tab: String {
+case Home = "home"
+case Heart = "heart"
+case ClipBoard = "clipboard"
+case Person = "person"
+    
+}
+
