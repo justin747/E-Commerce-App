bf0649d55e619cdfba6f43063ff5af4bcaa58fc5
Justin747 <justin747@gmail.com>
2021-09-04 00:11:05 -0700

    Added Custom Tab Bar
    Added Custom Tab Bar to bottom of screen.

--- E-Commerce App/View/BaseView.swift
+++ E-Commerce App/View/BaseView.swift
@@ -8,8 +8,59 @@
 import SwiftUI
 
 struct BaseView: View {
+    @StateObject var baseData = BaseViewModel()
+    
+    
+    init(){
+        
+        UITabBar.appearance().isHidden = true
+    }
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        TabView(selection: $baseData.currentTab){
+            
+            Text("Home")
+                .tag(Tab.Home)
+            
+            Text("Heart")
+                .tag(Tab.Heart)
+            
+            Text("Clipboard")
+                .tag(Tab.ClipBoard)
+            
+            Text("Person")
+                .tag(Tab.Person)
+            
+        }
+        .overlay(
+            //MARK: - Custom Tab Bar
+            HStack(spacing: 0){
+                TabButton(Tab: .Home)
+                TabButton(Tab: .Heart)
+                TabButton(Tab: .ClipBoard)
+                TabButton(Tab: .Person)
+            } ,alignment: .bottom
+        )
+    }
+    
+    @ViewBuilder func TabButton(Tab: Tab) -> some View{
+        
+        Button {
+            withAnimation{
+                baseData.currentTab = Tab
+            }
+        } label: {
+            Image(Tab.rawValue)
+                .resizable()
+                .renderingMode(.template)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 25, height: 25)
+                .foregroundColor(baseData.currentTab == Tab ? Color("DarkBlue") : Color.gray.opacity(0.5))
+                .frame(maxWidth: .infinity)
+        }
+        
     }
 }
 
