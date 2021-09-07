9e8e110c77f087a9a216c1f54ce2816f3d24f513
Justin747 <justin747@gmail.com>
2021-09-06 18:19:43 -0700

    Started CustomShape
    Started CustomShape

--- E-Commerce App/View/CustomCurveShape.swift
+++ E-Commerce App/View/CustomCurveShape.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct CustomCurveShape: View {
+struct CustomCurveShape: Shape {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
